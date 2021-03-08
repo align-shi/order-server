a19632c04fba8494c9d0430cbcd47700f717818e
shimenghao <shimenghao@kedacom.com>
2021-03-08 16:38:42 +0800

    配置静态文件可以访问

--- src/main/java/com/xiaoshi/config/CORSConfiguration.java
+++ src/main/java/com/xiaoshi/config/CORSConfiguration.java
@@ -2,10 +2,15 @@ package com.xiaoshi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class CORSConfiguration extends WebMvcConfigurationSupport {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
 
     /*
      * 这里主要为了解决跨域问题,所以重写addCorsMappings方法
@@ -23,5 +28,21 @@ public class CORSConfiguration extends WebMvcConfigurationSupport {
                         "X-Frame-Options")
                 .allowCredentials(false).maxAge(3600);
         super.addCorsMappings(registry);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        try {
+            if (!registry.hasMappingForPattern("/webjars/**")) {
+                registry.addResourceHandler("/webjars/**").addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/");
+            }
+            if (!registry.hasMappingForPattern("/**")) {
+                registry.addResourceHandler("/**").addResourceLocations(
+                        CLASSPATH_RESOURCE_LOCATIONS);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
